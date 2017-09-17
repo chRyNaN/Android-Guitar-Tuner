c697ff0afbe352f9378b28b6cf51b27e08e06515
Chris <ckeenan@starry.com>
2017-09-17 14:18:19 -0400

    Added support for Float PCM format

--- app/src/main/java/com/chrynan/android_guitar_tuner/tuner/config/AndroidAudioConfig.java
+++ app/src/main/java/com/chrynan/android_guitar_tuner/tuner/config/AndroidAudioConfig.java
@@ -2,6 +2,7 @@ package com.chrynan.android_guitar_tuner.tuner.config;
 
 import android.media.AudioFormat;
 import android.media.AudioRecord;
+import android.os.Build;
 
 /**
  * An Android implementation of the {@link AudioConfig} interface.
@@ -9,10 +10,18 @@ import android.media.AudioRecord;
 public class AndroidAudioConfig implements AudioConfig {
     private static final int AUDIO_RECORD_SAMPLE_RATE = 44100;
     private static final int AUDIO_RECORD_CHANNEL_CONFIG = AudioFormat.CHANNEL_IN_DEFAULT;
-    private static final int AUDIO_RECORD_AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT;
+    private static final int AUDIO_RECORD_AUDIO_FORMAT = getAudioFormat();
     private static final int AUDIO_RECORD_BUFFER_SIZE = AudioRecord.getMinBufferSize(AUDIO_RECORD_SAMPLE_RATE,
             AUDIO_RECORD_CHANNEL_CONFIG, AUDIO_RECORD_AUDIO_FORMAT);
     private static final int AUDIO_RECORD_READ_SIZE = AUDIO_RECORD_BUFFER_SIZE / 4;
+
+    private static int getAudioFormat() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return AudioFormat.ENCODING_PCM_FLOAT;
+        }
+
+        return AudioFormat.ENCODING_PCM_16BIT;
+    }
 
     public AndroidAudioConfig() {
         // Default constructor
